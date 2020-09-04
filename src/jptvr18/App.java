cc60a333e7a35246afc1b45eea5762c5c5865e53
pupil <pupil@ivkhk.ee>
2020-09-04 15:19:10 +0300

    Загатовка для проекта

--- src/jptvr18/App.java
+++ src/jptvr18/App.java
@@ -5,12 +5,45 @@
  */
 package jptvr18;
 
+import Entity.Recource;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  *
  * @author pupil
  */
 class App {
+    private List<Resource> listResources = new ArrayList<>();
     public void run(){
-        System.out.print("Hello");
+        System.out.println("---Менеджер паролей---");
+        String repeat = "r";
+        do{
+            System.out.println("Выберите задачу: ");
+            System.out.println("0 - выйти из программы");
+            System.out.println("1 - добавить ресурс");
+            System.out.println("2 - спискок ресурсов");
+            Scanner scanner = new Scanner(System.in);
+            int task = scanner.nextInt();
+            switch (task){
+                case 0:
+                    repeat = "q";
+                    break;
+                case 1:
+                    System.out.println("Выбрана задача 1 ");
+                    Recource recource = new Recource();
+                    recource.setName("ivkhk");
+                    recource.setUrl("http://kutsehariduskeskus.ee");
+                    break;
+                case 2:
+                    System.out.println("Выбрана задача 2 ");
+                    System.out.println("Список ресурсов: ");
+                    for(Recource r : listResources){
+                        System.out.println(r.toString());
+                    }
+                    break;
+            }
+        } while("q".equals(repeat));    
     }
 }
